3825d232267eb85af4e0c2f91a6f5285c8f1668a
S.Mohammed Rafeek <91260601+MohammedRafeek@users.noreply.github.com>
2022-11-01 21:08:53 +0530

    Add files via upload

---
+++ Assignments/Mohammed Rafeek/Assignment-1/Code/Pypipackage.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+df = pd.read_csv('Salary.csv')
+arr = np.array([[-1, 2, 0, 4],
+                [4, -0.5, 6, 0],
+                [2.6, 0, 7, 8],
+                [3, -7, 4, 2.0]])
+print("Initial Array: ")
+print(arr)
+
+sns.pairplot(df,hue="third",height=3)
+
+from pytz import timezone
+from datetime import datetime
+format = "%Y-%m-%d %H:%M:%S %Z%z"
+
+now_utc = datetime.now(timezone('UTC'))
+print(now_utc.strftime(format))
+
+now_asia = now_utc.astimezone(timezone('Asia/Kolkata'))
+print(now_asia.strftime(format))
+
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
